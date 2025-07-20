610699ddbb0cf7ceeb59d70eab4d7a9ee583ce4c
Erick Pérez <erick.gperez@outlook.com>
2025-07-20 11:03:31 -0600

    Diccionario de datos

--- public/dicc.php
+++ public/dicc.php
@@ -29,7 +29,10 @@ $query = "SELECT
     tc.table_name,
     kcu.column_name,
     tc.constraint_type,
-    pc.confrelid::regclass AS foreign_table_name
+    ccu.table_name as foreign_table_name,
+    ccu.column_name as foreign_column_name,
+    ccu.table_schema as foreign_schema_name,
+    pc.confupdtype, pc.confdeltype
 FROM
     information_schema.table_constraints AS tc
 JOIN
@@ -41,7 +44,7 @@ JOIN
     information_schema.constraint_column_usage AS ccu
 ON
     tc.constraint_name = ccu.constraint_name
-    AND tc.table_schema = ccu.table_schema
+    --AND tc.table_schema = ccu.table_schema
 JOIN
     pg_constraint AS pc ON pc.conname = tc.constraint_name
 JOIN
@@ -49,8 +52,18 @@ JOIN
     ";
 $result = pg_query($conn, $query);
 $restricciones = [];
-foreach (pg_fetch_all($result) as $r) {
-    $rel = $r['constraint_type'] === 'FOREIGN KEY' ? ' ON ' . $r['foreign_table_name'] . '(id)' : '';
+$res = pg_fetch_all($result);
+
+foreach ($res as $r) {
+    $rel = $r['constraint_type'] === 'FOREIGN KEY' ? ' ON ' . $r['foreign_schema_name'] . '.' . $r['foreign_table_name'] . '(' . $r['foreign_column_name'] . ')' : '';
+
+    $rel .= $r['confdeltype'] === 'r' ? ' ON DELETE RESTRICT' : '';
+    $rel .= $r['confdeltype'] === 'c' ? ' ON DELETE CASCADE' : '';
+    $rel .= $r['confdeltype'] === 'a' ? ' ON DELETE NO ACTION' : '';
+    $rel .= $r['confupdtype'] === 'r' ? ' ON UPDATE RESTRICT' : '';
+    $rel .= $r['confupdtype'] === 'c' ? ' ON UPDATE CASCADE' : '';
+    $rel .= $r['confupdtype'] === 'a' ? ' ON UPDATE NO ACTION' : '';
+
     $restricciones[$r['table_schema'] . '_' . $r['table_name'] . '_' . $r['column_name']][] = $r['constraint_type'] . $rel;
 }
 
@@ -71,11 +84,13 @@ foreach ($tablas as $row) {
 
 
     // Consulta para obtener las columnas de la tabla
-    $query_columnas = "SELECT column_name, data_type, dtd_identifier, character_maximum_length,
-                        udt_name, is_nullable
-
+    $query_columnas = "
+        SELECT column_name, data_type, dtd_identifier,
+                character_maximum_length, column_default,
+                udt_name, is_nullable, numeric_precision, numeric_scale
         FROM information_schema.columns
-        WHERE table_name = '" . $row['tablename'] . "';";
+        WHERE table_schema = '$row[schemaname]'
+            AND table_name = '$row[tablename]'";
     $result_columnas = pg_query($conn, $query_columnas);
 
     $idCol = 0;
@@ -94,6 +109,7 @@ foreach ($tablas as $row) {
                     <TH>Tipo</TH>
                     <TH>Nulo</TH>
                     <TH>Restricciones</TH>
+                    <TH>Valor por defecto</TH>
                     <TH>Descripci&oacute;n</TH>
                 </TR>
             </THEAD>
@@ -102,6 +118,8 @@ foreach ($tablas as $row) {
             $tipo = $row_columnas['data_type'];
             if ($row_columnas['udt_name'] === 'varchar') {
                 $tipo .= " ($row_columnas[character_maximum_length]) ";
+            } elseif ($row_columnas['udt_name'] === 'numeric') {
+                $tipo .= " ($row_columnas[numeric_precision], $row_columnas[numeric_scale]) ";
             }
             $nullable = $row_columnas['is_nullable'] === 'YES' ? 'S&iacute;' : 'No';
             $idCol = $row_columnas['dtd_identifier'];
@@ -118,6 +136,7 @@ foreach ($tablas as $row) {
                     <TD>$tipo</TD>
                     <TD>$nullable</TD>
                     <TD>$restriccionesCampo</TD>
+                    <TD>$row_columnas[column_default]</TD>
                     <TD>$comentario</TD>
                 </TR>";
         }
