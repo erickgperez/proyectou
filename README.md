538fe4d8e5888e310033a4ab65bae491664da26e
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2025-07-21 08:19:39 -0600

    Actualización archivo README

--- README.md
+++ README.md
@@ -1,32 +1,48 @@
-# Instalación
-## Clonar el repositorio
-``git clone git@github.com:erickgperez/proyectou.git``
+# Instalación y configuración del proyecto
+
+## Instalación de código fuente
+
+### Clonar el repositorio
+
+`git clone git@github.com:erickgperez/proyectou.git`
 
 Despues de clonar entrar al directorio en que se clonó el proyecto
-``cd proyectou``
+`cd proyectou`
 
-## Instalar dependencias php
-``composer install``
+### Instalar dependencias php
 
-## Instalar dependencias js
-``npm install && npm run build``
+`composer install`
 
-## Configurar la conexión a la base de datos
-Copiar el archivo de variables de entorno
-``cp .env.example .env``
+### Instalar dependencias js
+
+`npm install && npm run build`
+
+## Configuración inicial
+
+### Copiar el archivo de variables de entorno
+
+`cp .env.example .env`
+
+### Generar la llave del proyecto
+
+`php artisan key:generate`
+
+### Configurar la conexión a la base de datos
 
 Abrir el archivo `.env` y adecuar los valores
-``
-DB_CONNECTION=pgsql
+`DB_CONNECTION=pgsql
 DB_HOST=127.0.0.1
 DB_PORT=5432
 DB_DATABASE=usuariodb
 DB_USERNAME=proyectodb
-DB_PASSWORD=clavedb
-``
+DB_PASSWORD=clavedb`
 
+### Cargar la estructura de la base de datos
 
-## Iniciar el servidor de desarrollo
-``composer run dev``
+`php artisan migrate`
 
-Cargar el sitio en el navegador, verificar el puerto en que se ejecutó, por ejemplo: http://127.0.0.1:8000
+### Iniciar el servidor de desarrollo
+
+`composer run dev`
+
+Cargar el sitio en el navegador, verificar el puerto en que se ejecutó, por ejemplo: <http://127.0.0.1:8000>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,17 +1,36 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+</script>
+
 <template>
-    <v-layout class="rounded rounded-md border">
-        <v-navigation-drawer>
-            <v-list nav>
-                <v-list-item title="Navigation drawer" link></v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+    <Head title="Dashboard" />
 
-        <v-app-bar title="Application bar"></v-app-bar>
-
-        <v-main class="d-flex align-center justify-center" height="300">
-            <v-container>
-                <v-sheet border="dashed md" color="surface-light" height="200" rounded="lg" width="100%"></v-sheet>
-            </v-container>
-        </v-main>
-    </v-layout>
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+            </div>
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <PlaceholderPattern />
+            </div>
+        </div>
+    </AppLayout>
 </template>
